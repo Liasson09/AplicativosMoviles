f726a03224d201b7024aa9d086db9d9e8ffa097a
Liasson09 <113142326+Liasson09@users.noreply.github.com>
2023-09-05 22:36:25 -0500

    Add files via upload

---
+++ Semana42/app/src/main/java/utp/soria/semana4_2/dialog_add_note.java
@@ -0,0 +1,14 @@
+package utp.soria.semana4_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dialog_add_note extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_add_note);
+    }
+}
---
+++ Semana42/app/src/main/java/utp/soria/semana4_2/note_row.java
@@ -0,0 +1,14 @@
+package utp.soria.semana4_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class note_row extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_row);
+    }
+}
